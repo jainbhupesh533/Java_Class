49d309b7c4d766e91081b20a6f4baacacbdf5f9a
jainbhupesh533 <jainbhupesh533@gmail.com>
2020-01-19 21:57:18 +0530

    Selecting dishes

--- resto.java
+++ resto.java
@@ -2,12 +2,12 @@ import static java.lang.System.*;
 import java.util.*;
 
 class Restaurant {
-
-    int opt_dish_price[] = {};
-    int main_dish_price[] = {};
+    static Hashtable<String, Integer> cart = new Hashtable<String, Integer>();
     int dish_type;
     int type;
     int dish_no;
+    int item_no;
+    int another_ord;
     static String Resto_name = "ChopaChops";
 
     void dish_config() {
@@ -25,12 +25,12 @@ class Restaurant {
 
     int main_dish() {
         Hashtable<String, List<String>> main_dish_type = new Hashtable<String, List<String>>();
-        List<String> all_time_favorite = Arrays.asList("Cheese French Fries", "Cheese Garlic Bread", "Chilli Cheese Toast",
-                "Chilli Garlic Toast", "Cheese Garlic Bread");
-            out.println(all_time_favorite);
+        List<String> all_time_favorite = Arrays.asList("Cheese French Fries", "Cheese Garlic Bread",
+                "Chilli Cheese Toast", "Chilli Garlic Toast", "Cheese Garlic Bread");
+        out.println(all_time_favorite);
         List<String> sandwich = Arrays.asList("Plain Sandwich", "Grill Sandwich", "Club Sandwich");
-        List<String> main_course = Arrays.asList("Mix. Veg.in garlic","Veg Chopsouey sauce", "Shreded Potatoes", "Manchurian Gravy",
-                "Sweet & Sour Veg", "Chilly Paneer Gravy");
+        List<String> main_course = Arrays.asList("Mix. Veg.in garlic", "Veg Chopsouey sauce", "Shreded Potatoes",
+                "Manchurian Gravy", "Sweet & Sour Veg", "Chilly Paneer Gravy");
         {
             main_dish_type.put("All Time Favorite", all_time_favorite);
             main_dish_type.put("Sandwich", sandwich);
@@ -54,35 +54,70 @@ class Restaurant {
         Scanner obj1 = new Scanner(System.in);
         dish_no = obj1.nextInt();
         out.println("Item \t\t\t Price");
+        int j = 1;
         switch (dish_no) {
         case 1:
             for (int i = 0; i <= main_course.size() - 1; i++) {
-                out.println(main_course.get(i) + "\t" + main_course_pr.get(i));
+                out.println(j + "\t" + main_course.get(i) + "\t" + main_course_pr.get(i));
+                j++;
             }
             break;
         case 2:
             for (int i = 0; i <= sandwich.size() - 1; i++) {
-                out.println(sandwich.get(i) + "\t\t\t" + sandwich_pr.get(i));
+                out.println(j + "\t" + sandwich.get(i) + "\t\t\t" + sandwich_pr.get(i));
+                j++;
             }
             break;
         case 3:
-            for (int i = 0; i <= all_time_favorite.size() - 1 ; i++) {
-                out.println(all_time_favorite.get(i) + "\t" + all_time_favorite_pr.get(i));
+            for (int i = 0; i <= all_time_favorite.size() - 1; i++) {
+                out.println(j + "\t" + all_time_favorite.get(i) + "\t" + all_time_favorite_pr.get(i));
+                j++;
             }
             break;
         default:
             break;
         }
+        out.println("Please Choose Your Food by pressing item no: ");
+        item_no = obj1.nextInt();
+        if (dish_no == 1) {
+            cart.put(main_course.get(item_no - 1), main_course_pr.get(item_no - 1));
+        } else if (dish_no == 2) {
+            cart.put(sandwich.get(item_no - 1), sandwich_pr.get(item_no - 1));
+        } else if (dish_no == 3) {
+            cart.put(all_time_favorite.get(item_no - 1), all_time_favorite_pr.get(item_no - 1));
+        } else {
+            out.println("Press 1 for this menu");
+            out.println("Press 2 for this healthy menu like salad");
+            out.println("Press 3 for Checkout");
+            another_ord = obj1.nextInt();
+            type = (another_ord == 1) ? main_dish() : (another_ord == 2) ? healthy_dish() : final_out();
+        }
+        out.println("Selected item: ");
+        out.println("Item \t\t\t Price");
+        for (Map.Entry<String, Integer> m : cart.entrySet()) {
+            System.out.println(m.getKey() + "\t" + m.getValue());
+        }
+        out.println("Would You like to Anything more: ");
+        out.println("Press 1 for this menu");
+        out.println("Press 2 for this healthy menu like salad");
+        out.println("Press 3 for Checkout");
+        another_ord = obj1.nextInt();
+        type = (another_ord == 1) ? main_dish() : (another_ord == 2) ? healthy_dish() : final_out();
         return 0;
     }
 
     int healthy_dish() {
-        
+
         return 1;
+    }
+
+    int final_out() {
+        return 2;
     }
 
     public static void main(String[] args) {
         Restaurant r1 = new Restaurant();
         r1.dish_config();
+
     }
 }
